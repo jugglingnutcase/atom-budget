a61d0fc6101bdcd71e130445729fc0f88b47e526
James R Sconfitto <jugglingnutcase@gmail.com>
2014-06-27 13:11:21 -0400

    Add level-js package
    level-js utilizes IndexedDB, which atom has as well. Fortunately for me,
    it appears that the data persists across launches. That's very nice.

--- budget/index.js
+++ budget/index.js
@@ -1,1 +1,13 @@
 var $ = require('jquery');
+var leveljs = require('level-js');
+var db = leveljs('account');
+
+db.open(function() {
+  db.put('user', 'james', function() {
+    console.log("Added user");
+  })
+
+  db.get('user', function(data) {
+    console.log("Found: user=" + "james");
+  });
+});
