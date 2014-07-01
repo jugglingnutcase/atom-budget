66c2b817f5378c40fdf4dbdc22603e7c01e33cad
James R Sconfitto <jugglingnutcase@gmail.com>
2014-07-01 09:12:21 -0400

    Add ability to save & load accounts

--- budget/index.js
+++ budget/index.js
@@ -1,13 +1,50 @@
 var $ = require('jquery');
 var leveljs = require('level-js');
-var db = leveljs('account');
+var db = leveljs('budget');
 
-db.open(function() {
-  db.put('user', 'james', function() {
-    console.log("Added user");
+function addAccount() {
+  var accounts = getAccountsFromUI();
+  var newAccount = { name: $('#accountName').val() }
+  accounts.push(newAccount);
+  storeAccounts(accounts);
+}
+
+function getAccountsFromUI() {
+  var accounts = [];
+  $('.accounts .account').each(function () {
+    accounts.push({ name: $(this).data('name') })
   })
 
-  db.get('user', function(data) {
-    console.log("Found: user=" + "james");
+  return accounts;
+}
+
+// Build up and insert the HTML
+function fillAccounts(accounts) {
+  var accountsHTML = accounts.map(function (account) {
+    return '<li class="account" data-name="' + account.name + '">' + account.name + '</li>'
+  })
+  $('.accounts').html(accountsHTML)
+}
+
+function storeAccounts(accounts) {
+  db.put('accounts', JSON.stringify(accounts), function(err) {
+    if (err) {
+      console.error('Error saving accounts: ' + err);
+      return;
+    }
+
+    fillAccounts(accounts);
   });
-});
+}
+
+function appReady() {
+  db.get('accounts', function(err, data) {
+    if (err) return;
+    fillAccounts(JSON.parse(data.toString()));
+  });
+
+  $('.addAccount').on('click', addAccount);
+}
+
+// Prefill any accounts we know about
+db.open(appReady);
