280111349e4cf7b5bc1292e8e99ec73f4102e4eb
James R Sconfitto <jugglingnutcase@gmail.com>
2014-07-04 13:06:46 -0400

    Add needed build steps

--- script/build.ps1
+++ script/build.ps1
@@ -1,4 +1,12 @@
+# Build the atom shell
 Push-Location $PSscriptRoot\..\build
 & npm install
 & grunt download-atom-shell
 Pop-Location
+
+# Build the app
+Push-Location $PSscriptRoot\..\budget
+& npm install
+& bower install
+Pop-Location
+
