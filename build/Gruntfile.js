d3a7558c4189ef276f0e950a542d22a2e377ca28
James R Sconfitto <jugglingnutcase@gmail.com>
2014-07-22 09:53:20 -0400

    Upgrade to latest atom-shell

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     'download-atom-shell': {
-      version: '0.13.3',
+      version: '0.14.0',
       outputDir: 'atom-shell',
       downloadDir: atomShellDownloadDir,
       rebuild: true
