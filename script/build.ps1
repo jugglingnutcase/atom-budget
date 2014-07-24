7914949ade00dc2f972cfe1fe89e9f4bc6016c00
James R Sconfitto <jugglingnutcase@gmail.com>
2014-07-24 11:38:18 -0400

    Add comment

--- script/build.ps1
+++ script/build.ps1
@@ -6,6 +6,8 @@ Pop-Location
 
 # Build the app
 Push-Location $PSscriptRoot\..\budget
+
+# Install app dependencies via apm and bower
 & ..\build\node_modules\atom-package-manager\bin\apm.cmd install . --arch=ia32
 & bower install
 Pop-Location
