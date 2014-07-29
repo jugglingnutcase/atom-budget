e03ff9de12ca713a745a61c87d2a913bfec30e27
James R Sconfitto <jugglingnutcase@gmail.com>
2014-07-29 10:30:45 -0400

    Add and use atom-shell field in package.json

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,12 +1,15 @@
 var os = require('os');
 var path = require('path');
+var packageJSON = require('./package.json');
 
 module.exports = function(grunt) {
   var atomShellDownloadDir = path.join(os.tmpdir(), 'atom-cached-atom-shells')
 
+  console.log(process.cwd());
+
   grunt.initConfig({
     'download-atom-shell': {
-      version: '0.14.0',
+      version: packageJSON.atomShellVersion,
       outputDir: 'atom-shell',
       downloadDir: atomShellDownloadDir,
       rebuild: true
